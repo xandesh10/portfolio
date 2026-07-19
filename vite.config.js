bfee3a9415a1c9fedfdb35100f434fbf4be1addd
xandesh10 <xandeshbk@gmail.com>
2026-07-19 14:34:17 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/portfolio/',
    server: {
     host: "0.0.0.0", // Allow connections from any host
     port: 5173,
